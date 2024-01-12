4bec9f97e2c989b764e64ed7e9b41230f23a7e0a
Bilaxten <98403014+Bilaxten@users.noreply.github.com>
2024-01-12 21:35:30 +0300

    Flip

--- scripts/player.gd
+++ scripts/player.gd
@@ -6,10 +6,14 @@ var friction = 0.2
 
 func _ready():
 	animated_sprite_2d.play("default")
+	
 func _process(delta: float) -> void:
 	velocity=joystick.output * player_stats.move_speed
 	move_and_slide()
 	self.look_at(self.position + joystick.output) #Movement vektör ile rotation takibi
 	
 	if joystick.is_pressed == false:
-		joystick.output = joystick.output.move_toward(Vector2.ZERO, friction * delta * player_stats.move_speed) #inertia??? olmadı.
+		joystick.output = joystick.output.move_toward(Vector2.ZERO, friction * delta * player_stats.move_speed) #inertia???, olmadı.
+
+	if joystick.output.x < 0:
+		$Player.flip_h
