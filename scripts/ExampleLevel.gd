7018675a956055c3ebf78eb479d1492ccd9de5d6
xBlippy <akinkiziltepe46@gmail.com>
2024-01-23 23:44:33 +0300

    title screen
    title screen

--- scripts/ExampleLevel.gd
+++
@@ -1,9 +0,0 @@
-extends StaticBody2D
-
-@onready var collision_polygon_2d = $CollisionPolygon2D
-@onready var polygon_2d = $Polygon2D
-
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	polygon_2d.polygon=collision_polygon_2d.polygon
